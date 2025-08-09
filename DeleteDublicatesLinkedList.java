6025c4b23a9a731923947ffe3ed56f78ea53687d
Jb192101 <grigorijbaryshev910@gmail.com>
2025-08-09 11:21:06 +0300

    Задача Delete dublicates from linked list leetcode

---
+++ DeleteDublicatesLinkedList.java
@@ -0,0 +1,16 @@
+// Link: https://leetcode.com/problems/remove-duplicates-from-sorted-list/description
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode res = head;
+
+        while (head != null && head.next != null) {
+            if (head.val == head.next.val)
+                head.next = head.next.next;
+            else
+                head = head.next;
+        }
+
+        return res;
+    }
+}
